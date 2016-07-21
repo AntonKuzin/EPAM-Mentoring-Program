8543d1141d04769b25b15b3fec19a74707d4c1b4
Anton Kuzin <anton.bfg.kuzin@gmail.com>
2016-07-22 00:32:02 +0300

    Task 2

---
+++ Task2.Type Fundamentals/Library/Methods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public static class Methods
+    {
+        public static int Calculate(int count)
+        {
+            return 0;
+        }
+    }
+}
