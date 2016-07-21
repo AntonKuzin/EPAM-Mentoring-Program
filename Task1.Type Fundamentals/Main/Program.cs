23fd9fb3fecbe93c2079f812cba43886aab52188
Anton Kuzin <anton.bfg.kuzin@gmail.com>
2016-07-22 00:12:35 +0300

    First version

---
+++ Task1.Type Fundamentals/Main/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Types;
+
+namespace Main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new Person("test", "sample");
+            var p2 = new Person("test", "sample");
+
+            Console.WriteLine(p1.Equals(p2));
+        }
+    }
+}
---
+++ Task1.Type Fundamentals/Types/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Types
+{
+    public class Person: IEquatable<Person>
+    {
+        public Person(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public bool Equals(Person other)
+        {
+            if (other == null)
+                return false;
+            return other.FirstName == FirstName && other.LastName == LastName;
+        }
+    }
+}
