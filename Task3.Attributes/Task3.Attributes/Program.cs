24a5ca664a68df7b5c443b50123318a46d6cef69
Anton Kuzin <anton.bfg.kuzin@gmail.com>
2016-08-17 18:26:20 +0300

    Attributes module homework

---
+++ Task3.Attributes/Task3.Attributes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3.Attributes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var instance = new TestClass();
+            Type type = typeof(TestClass);
+            var attrs = type.GetCustomAttributes(false);
+
+            foreach (var roleAttr in attrs)
+            {
+                Console.WriteLine(roleAttr);
+            }
+
+        }
+    }
+}
---
+++ Task3.Attributes/Task3.Attributes/TestClass.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3.Attributes
+{
+    [Author(Name = "Anton Kuzin", Email = "anton_kuzin@epam.com")]
+    public class TestClass
+    {
+    }
+}
